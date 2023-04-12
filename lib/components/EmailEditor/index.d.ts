30fe7b207b67b78e7da32149aefb8dfb49e52e60
An Duong <an.duong@momos.io>
2023-04-12 16:09:55 +0700

    chore: upgrade to 4.7.0

---
+++ lib/components/EmailEditor/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+import '../../assets/font/iconfont.css';
+declare type EmailEditorProps = {
+    extraTop?: React.ReactNode;
+};
+export declare const EmailEditor: ({ extraTop }: EmailEditorProps) => JSX.Element;
+export {};
---
+++ lib/components/Provider/EmailEditorProvider/index.d.ts
@@ -0,0 +1,13 @@
+import { IEmailTemplate } from '../../../typings';
+import React from 'react';
+import { ActiveTabKeys } from '..//BlocksProvider';
+import { PropsProviderProps } from '../PropsProvider';
+import { Config, FormApi, FormState } from 'final-form';
+export interface EmailEditorProviderProps<T extends IEmailTemplate = any> extends PropsProviderProps {
+    data: T;
+    children: (props: FormState<T>, helper: FormApi<IEmailTemplate, Partial<IEmailTemplate>>) => React.ReactNode;
+    onSubmit?: Config<IEmailTemplate, Partial<IEmailTemplate>>['onSubmit'];
+    validationSchema?: Config<IEmailTemplate, Partial<IEmailTemplate>>['validate'];
+    defaultActiveTab?: ActiveTabKeys;
+}
+export declare const EmailEditorProvider: <T extends unknown>(props: EmailEditorProviderProps<any> & T) => JSX.Element | null;
